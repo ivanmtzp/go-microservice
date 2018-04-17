309d3ab5c97325ba1d7aafbbb38aa1f32efb8c75
ivan <ivanmtzp@gmail.com>
2018-04-18 01:32:13 +0200

    11111

--- server.go
+++ server.go
@@ -3,13 +3,13 @@ package microservice
 import (
 	"fmt"
 	"time"
+	"strconv"
 
 	"github.com/gobuffalo/pop"
-
 	"github.com/ivanmtzp/go-microservice/grpc"
 	"github.com/ivanmtzp/go-microservice/log"
-	"github.com/ivanmtzp/go-microservice/config"
 	"github.com/ivanmtzp/go-microservice/metrics"
+
 )
 
 
@@ -17,7 +17,8 @@ type MicroService struct {
 	name string
 	settings SettingsReader
 	grpcServer *grpc.Server
-
+	database *pop.Connection
+	optionalMetricsPusher bool
 }
 
 
@@ -26,27 +27,40 @@ func New(name string, sr SettingsReader) *MicroService {
 }
 
 func (ms *MicroService) WithGrpcAndGateway(sr grpc.ServiceRegistrator,gsr grpc.HttpGatewayServiceRegistrator) *MicroService {
-	ms.grpcServer = grpc.New(ms.settings.GrpcSettings().address, sr).
-		WithGateway(ms.settings.GrpcSettings().gatewayAddress, gsr)
+	ms.grpcServer = grpc.New(ms.settings.Grpc().address, sr).
+		WithGateway(ms.settings.Grpc().gatewayAddress, gsr)
+	return ms
+}
+
+func (ms *MicroService) WithDatabase() (*MicroService, error) {
+	dbSettings := ms.settings.Database()
+	connectionDetails := &pop.ConnectionDetails{	Dialect: dbSettings.dialect, Database: dbSettings.database,
+		Host: dbSettings.host, Port: strconv.Itoa(dbSettings.port), User: dbSettings.user, Password: dbSettings.password,
+		Pool: dbSettings.pool, IdlePool: 0}
+	connection, err := pop.NewConnection(connectionDetails)
+	if err != nil {
+		return ms, fmt.Errorf("failed to create the database connection %s", err)
+	}
+	ms.database = connection
+	return ms, nil
+}
+
+func (ms *MicroService) WithOptionalMetricsPusher() *MicroService {
+	ms.optionalMetricsPusher = true
 	return ms
 }
 
 
 func (ms *MicroService) Run() {
 
-	connectionDetails := &pop.ConnectionDetails{	Dialect: "postgres", Database: "marketdatacurve",
-		Host: "localhost", Port: "5432", User: "postgres", Password: "postgres",
-		Pool: 10, IdlePool: 0}
-
-	db, err := pop.NewConnection(connectionDetails)
-	if err != nil {
-		log.FailOnError(err, "failed to connect to database")
+	if ms.database != nil {
+		log.Info("connecting to database ", ms.database.URL())
+		err := ms.database.Open()
+		if err != nil {
+			log.FailOnError(err, "couldn't open connection to database ")
+		}
+		defer ms.database.Close()
 	}
-	err = db.Open()
-	if err != nil {
-		log.FailOnError(err, "couldn't open connection to database ")
-	}
-	defer db.Close()
 
 	// err = handlers.PingDb(db)
 	// if err != nil {
@@ -56,31 +70,26 @@ func (ms *MicroService) Run() {
 	if ms.grpcServer != nil {
 		// fire the gRPC server in a goroutine
 		go func() {
-			log.Infof("starting HTTP/2 gRPC server on %s", ms.settings.GrpcSettings().address)
+			log.Infof("starting HTTP/2 gRPC server on %s", ms.settings.Grpc().address)
 			err:= ms.grpcServer.Run()
-			log.FailOnError(err, fmt.Sprint("failed to start gRPC server " , ms.settings.GrpcSettings().address))
+			log.FailOnError(err, fmt.Sprint("failed to start gRPC server " , ms.settings.Grpc().address))
 		}()
 		// fire the http grpc gateway in a goroutine
 		go func() {
-			log.Infof("starting HTTP/1.1 REST server on %s", ms.settings.GrpcSettings().gatewayAddress)
+			log.Infof("starting HTTP/1.1 REST server on %s", ms.settings.Grpc().gatewayAddress)
 			err := ms.grpcServer.RunHttpGateway()
-			log.FailOnError(err, fmt.Sprint("failed to start Http gateway server ", ms.settings.GrpcSettings().gatewayAddress))
+			log.FailOnError(err, fmt.Sprint("failed to start Http gateway server ", ms.settings.Grpc().gatewayAddress))
 		}()
 	}
 
 	//	fire the metrics pusher
-	metricsPushEnabled := config.GetBool("metrics", "push_enabled")
-	if metricsPushEnabled {
+	if ms.optionalMetricsPusher {
 		go func() {
-			metricsInterval := config.GetInt("metrics", "interval")
-			metricsHostUrl := fmt.Sprintf("http://%s:%d", config.GetString("metrics", "host"),
-				config.GetString("metrics", "port"))
-			metricsDatabase := config.GetString("metrics", "database")
-			metricsUser := config.GetString("metrics", "user")
-			metricsPassword := config.GetString("metrics", "password")
-			log.Infof("starting InfluxDb Metrics pushing to: %s, database: %s, user: %s,  with interval: %d", metricsHostUrl,
-				metricsDatabase, metricsUser, metricsInterval)
-			metrics.StartInfluxDbPusher(time.Second * time.Duration(metricsInterval), metricsHostUrl, metricsDatabase, metricsUser, metricsPassword)
+			mps := ms.settings.MetricsPusher()
+			hostUrl := fmt.Sprintf("http://%s:%d", mps.host, mps.port)
+			log.Infof("starting InfluxDb Metrics pushing to: %s, database: %s, user: %s,  with interval: %d", hostUrl,
+				mps.database, mps.user, mps.interval)
+			metrics.StartInfluxDbPusher(time.Second * time.Duration(mps.interval), hostUrl, mps.database, mps.user, mps.password)
 		}()
 	}
 
--- settings.go
+++ settings.go
@@ -7,8 +7,10 @@ import (
 )
 
 type SettingsReader interface {
-	GrpcSettings() *GrpcSettings
-	LogSettings() *LogSettings
+	Grpc() *GrpcSettings
+	Log() *LogSettings
+	Database() *DatabaseSettings
+	MetricsPusher() *MetricsPusherSettings
 }
 
 type GrpcSettings struct {
@@ -20,11 +22,31 @@ type LogSettings struct {
 	level string
 }
 
+type DatabaseSettings struct {
+	dialect string
+	database string
+	host string
+	port int
+	user string
+	password string
+	pool int
+}
+
+type MetricsPusherSettings struct {
+	enabled bool
+	interval int
+	host string
+	port int
+	database string
+	user string
+	password string
+}
+
 type ConfigFileSettings struct {
 
 }
 
-func (c *ConfigFileSettings) GrpcSettings() *GrpcSettings {
+func (c *ConfigFileSettings) Grpc() *GrpcSettings {
 	host := config.GetString("grpc", "host")
 	grpcPort := config.GetInt("grpc", "port")
 	gatewayPort := config.GetInt("grpc", "gateway_port")
@@ -34,10 +56,38 @@ func (c *ConfigFileSettings) GrpcSettings() *GrpcSettings {
 	return &GrpcSettings{address: address, gatewayAddress: gatewayAddress}
 }
 
-func (c *ConfigFileSettings) LogSettings() *LogSettings {
+func (c *ConfigFileSettings) Log() *LogSettings {
 	level := config.GetString("log", "level")
 
 	return &LogSettings{level: level}
 }
+
+func (c *ConfigFileSettings) Database() *DatabaseSettings {
+	dialect := config.GetString("database", "dialect")
+	database := config.GetString("database", "name")
+	host := config.GetString("database", "host")
+	port := config.GetInt("database", "port")
+	user := config.GetString("database", "user")
+	password := config.GetString("database", "password")
+	pool := config.GetInt("database", "pool")
+
+	return &DatabaseSettings{dialect: dialect, database: database, host: host, port: port, user: user, password: password, pool: pool}
+}
+
+func (c *ConfigFileSettings) MetricsPusher() *MetricsPusherSettings {
+	enabled := config.GetBool("metrics", "pusher", "enabled")
+	if !enabled {
+		return &MetricsPusherSettings{}
+	}
+	interval := config.GetInt("metrics", "pusher", "interval")
+	host := config.GetString("metrics",  "pusher", "host")
+	port := config.GetInt("metrics", "pusher", "port")
+	database := config.GetString("metrics",  "pusher", "database")
+	user := config.GetString("metrics",  "pusher", "user")
+	password := config.GetString("metrics", "password")
+
+	return &MetricsPusherSettings{enabled: enabled, interval: interval, host: host, port: port, database: database, user: user, password: password}
+}
+
 
 
