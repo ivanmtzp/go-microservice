5d5c10ca25aea198903b978f713854d77d5fdc96
ivan <ivanmtzp@gmail.com>
2018-05-04 15:56:28 +0200

    111

--- broker/rabbitmq/rabbitmq.go
+++ broker/rabbitmq/rabbitmq.go
@@ -1,70 +1,50 @@
-package rabbitmq
+package broker
 
 import (
 	"github.com/streadway/amqp"
-	"github.com/ivanmtzp/go-microservice/log"
-	"fmt"
 )
 
-struct Broker
+type RabbitMqBroker struct
 {
-
+	address string
+	connection *amqp.Connection
+	channel *amqp.Channel
+	queues map[string]*amqp.Queue
+	consumerChannels map[string]<-chan amqp.Delivery
 }
 
-
-func NewRabbitmqBroker() *Broker {
-	return &Broker{}
+func NewRabbitMqBroker(address string, prefetchCount, prefetchSize int) (*Broker, error) {
+	connection, err := amqp.Dial(address)
+	if err != nil {
+		return nil, err
+	}
+	channel, err := connection.Channel()
+	if err != nil {
+		return nil, err
+	}
+	err = channel.Qos(prefetchCount, prefetchSize, false)
+	if err != nil {
+		return nil, err
+	}
+	return &RabbitMqBroker{address: address, connection: connection, channel:channel, queues: make(map[string]*amqp.Queue)}, nil
 }
 
-
-
-func initRabbitmqConnection()(*amqp.Connection, *amqp.Channel, *amqp.Queue, <-chan amqp.Delivery, error)  {
-
-	// rabbitUrl := viper.Get( "rabbitmq_url").(string)
-	rabbitUrl := "amqp://localhost:5672/"
-	log.Infof("initializing rabbitmq on %s", rabbitUrl)
-
-	conn, err := amqp.Dial(rabbitUrl)
-	failOnError(err, fmt.Sprintf("failed to connect to RabbitMQ"))
-
-	ch, err := conn.Channel()
-	failOnError(err, "failed to open a rabbitmq channel")
-
-	queue, err := ch.QueueDeclare(
-		"vol_calibrator_queue", // name
-		true,   // durable
-		false,   // delete when usused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
+func (b *RabbitMqBroker) WithQueue(name string, durable, autoDelete, exclusive, noWait bool) (*amqp.Queue, error) {
+	queue, err := b.channel.QueueDeclare( name, durable, autoDelete, exclusive, noWait, nil)
 	if err != nil {
-		return nil, nil, nil, nil, fmt.Errorf("failed to declare rabbitmq queue named vol_calibrator_queue, %s", err)
+		return nil, err
 	}
+	b.queues[name] = &queue
+	return &queue, nil
+}
 
-	err = ch.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
+func (b *RabbitMqBroker) WithConsumerChannel(queueName, consumer string, autoAck, exclusive, noLocal, noWait bool) (<-chan amqp.Delivery, error) {
+	consumerChannel, err := b.channel.Consume(queueName, consumer, autoAck, exclusive, noLocal, noWait, nil)
 	if err != nil {
-		return nil, nil, nil, nil, fmt.Errorf("failed to set rabbitmq channel QoS, %s", err)
+		return nil, err
 	}
-
-	msgsChannel, err := ch.Consume(
-		queue.Name, // queue
-		"",     // consumer
-		false,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	if err != nil {
-		return nil, nil, nil, nil, fmt.Errorf("failed to register a rabbitmq consumer, %s", err)
-	}
-
-	return conn, ch, &queue, msgsChannel, nil
+	b.consumerChannels[queueName] = consumerChannel
+	return consumerChannel, nil
 }
 
 
